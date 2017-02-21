2f0047d44b540a806c6201417888f4e800207a1d
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-02-21 16:22:55 +0000

    new clone

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170221152138) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +98,8 @@ ActiveRecord::Schema.define(version: 20170221152138) do
     t.string   "phone_number"
     t.string   "fullname"
     t.text     "description"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "booking_slots", "bookings"
