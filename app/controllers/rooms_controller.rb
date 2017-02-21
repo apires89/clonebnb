365f03b9586f4f4c0cc17fafde29ed67e44152b4
andre pires <andrecabralpires@gmail.com>
2017-02-21 11:51:23 +0000

    added new migrations and did rooms private controllers - Check migrations pls

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,33 @@
+class RoomsController < ApplicationController
+   before_action :set_room, only: [:show, :edit, :update]
+  def new
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(
+      :home_type, :room_type, :accomodate,
+     :bedrooms, :bathrooms, :summary, :address, :has_tv,
+     :has_wifi, :has_kitchen, :has_heating, :has_aircon, :price, :activate )
+  end
+end
