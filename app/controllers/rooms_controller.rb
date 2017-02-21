5598ea96f75c6ebd77578af09ff475698eecbc82
andre pires <andrecabralpires@gmail.com>
2017-02-21 12:37:53 +0000

    removed some things on rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,39 +1,27 @@
 class RoomsController < ApplicationController
-<<<<<<< HEAD
-   before_action :set_room, only: [:show, :edit, :update]
-=======
->>>>>>> e20437a0164e48184732da60475325efdc116d42
+before_action :set_room, only: [:show, :edit, :update]
   def new
   end
-
   def show
   end
-
   def index
   end
-
   def create
   end
-
   def update
   end
-
   def edit
   end
-<<<<<<< HEAD
 
   private
 
   def set_room
-    @room = Room.find(params[:id])
+  @room = Room.find(params[:id])
   end
-
   def room_params
-    params.require(:room).permit(
-      :home_type, :room_type, :accomodate,
-     :bedrooms, :bathrooms, :summary, :address, :has_tv,
-     :has_wifi, :has_kitchen, :has_heating, :has_aircon, :price, :activate )
+  params.require(:room).permit(
+  :home_type, :room_type, :accomodate,
+  :bedrooms, :bathrooms, :summary, :address, :has_tv,
+  :has_wifi, :has_kitchen, :has_heating, :has_aircon, :price, :activate )
   end
-=======
->>>>>>> e20437a0164e48184732da60475325efdc116d42
 end
