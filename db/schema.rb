8c95b1a4904cfb471b040f606617dd4df5857b1e
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-02-21 15:24:17 +0000

    facebook-connect-finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170220182927) do
+ActiveRecord::Schema.define(version: 20170221123952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,14 +70,19 @@ ActiveRecord::Schema.define(version: 20170220182927) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
 
   add_foreign_key "booking_slots", "bookings"
   add_foreign_key "booking_slots", "rooms"
   add_foreign_key "bookings", "users"
   add_foreign_key "rooms", "users"
-
 end
