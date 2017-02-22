91746669b049417ad80ed4c966fb370384267488
andre pires <andrecabralpires@gmail.com>
2017-02-22 12:15:18 +0000

    started figuring out geolocation, added gems and migrations

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,8 @@ class Room < ApplicationRecord
   belongs_to :user
   has_many :booking_slots, dependent: :destroy
   has_attachments :photos, maximum: 10
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 
   validates :home_type, :room_type, :accomodate, :bedrooms, :bathrooms, :summary, :address, presence: true
   validates :summary, length: {maximum: 500}
