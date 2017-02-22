06c0b9d7852b8c755bbb6ef10149ec1eb6b338c3
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-02-22 10:45:00 +0000

    footer finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-   def configure_permitted_parameters
+  # method to disable partials in specfic controllers
+  before_action :disable_nav, if: :devise_controller?
+
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,        keys: [:avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
+
+  # method to disable partials in specfic controllers
+  def disable_nav
+    @disable_nav = true
   end
 end
