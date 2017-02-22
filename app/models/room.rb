215f7a208c75d74826e07b3d644bccb8f87ef0a9
Turo Hakulinen <turo.hakulinen@gmail.com>
2017-02-22 20:32:28 +0000

    create room with date + date validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,7 +6,7 @@ class Room < ApplicationRecord
   has_many :booking_slots, dependent: :destroy
   has_attachments :photos, maximum: 10
 
-  validates :home_type, :room_type, :accomodate, :bedrooms, :bathrooms, :summary, :address, presence: true
+  validates :name, :home_type, :room_type, :accomodate, :bedrooms, :bathrooms, :summary, :address, presence: true
   validates :summary, length: {maximum: 500}
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221191634) do
+ActiveRecord::Schema.define(version: 20170222195159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 20170221191634) do
     t.boolean  "activate"
     t.string   "photo"
     t.string   "url"
+    t.string   "name"
     t.index ["user_id"], name: "index_rooms_on_user_id", using: :btree
   end
 
