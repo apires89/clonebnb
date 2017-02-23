5c561e5fbabf654d35b9f78fc8fb86a9cf569562
andre pires <andrecabralpires@gmail.com>
2017-02-23 09:08:21 +0000

    started to look into map styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170222184601) do
-
+ActiveRecord::Schema.define(version: 20170222195159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,7 +83,6 @@ ActiveRecord::Schema.define(version: 20170222184601) do
     t.boolean  "activate"
     t.string   "photo"
     t.string   "url"
-    t.string   "name"
     t.float    "latitude"
     t.float    "longitude"
     t.string   "name"
@@ -105,6 +102,9 @@ ActiveRecord::Schema.define(version: 20170222184601) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone_number"
+    t.string   "fullname"
+    t.text     "description"
     t.string   "provider"
     t.string   "uid"
     t.string   "facebook_picture_url"
@@ -112,9 +112,6 @@ ActiveRecord::Schema.define(version: 20170222184601) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
-    t.string   "phone_number"
-    t.string   "fullname"
-    t.text     "description"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
