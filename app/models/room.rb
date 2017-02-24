55dae089c7cad7afe142e896a4f35aa13aea85c0
andre pires <andrecabralpires@gmail.com>
2017-02-24 10:47:29 +0000

    algolia

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,21 @@
 class Room < ApplicationRecord
+  include AlgoliaSearch
+
+  algoliasearch do
+    # list of attribute used to build an Algolia record
+    attributes :name, :address, :description
+
+    # the `searchableAttributes` (formerly known as attributesToIndex) setting defines the attributes
+    # you want to search in: here `title`, `subtitle` & `description`.
+    # You need to list them by order of importance. `description` is tagged as
+    # `unordered` to avoid taking the position of a match into account in that attribute.
+    searchableAttributes ['name', 'address', 'unordered(description)']
+
+    # the `customRanking` setting defines the ranking criteria use to compare two matching
+    # records in case their text-relevance is equal. It should reflect your record popularity.
+    #customRanking ['desc(likes_count)']
+  end
+
   attr_accessor :start_date
   attr_accessor :end_date
   attr_accessor :price
