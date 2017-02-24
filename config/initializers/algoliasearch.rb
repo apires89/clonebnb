eec9bc2b5f68d7e3d5db34e0e07d401b43b25e36
andre pires <andrecabralpires@gmail.com>
2017-02-24 00:51:06 +0000

    algolia definitions - tomorrow implement

---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,11 @@
+AlgoliaSearch.configuration = { application_id: "VWALH7148H", api_key: "e5b8db1cf20a5674af2755866a9a357c" }
+
+# AlgoliaSearch.configuration = {
+#   application_id: "VWALH7148H",
+#   api_key: "e5b8db1cf20a5674af2755866a9a357c"
+#   connect_timeout: 2,
+#   receive_timeout: 30,
+#   send_timeout: 30,
+#   batch_timeout: 120,
+#   search_timeout: 5
+# }
